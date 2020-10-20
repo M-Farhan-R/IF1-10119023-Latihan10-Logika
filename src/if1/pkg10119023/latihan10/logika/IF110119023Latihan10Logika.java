ebb2ae10cd703817109189f9f24ae98275e33c62
Farhan <farhan.10119023@mahasiswa.unikom.ac.id>
2020-10-20 20:15:45 +0700

    Latihan 10 - Logika

---
+++ src/if1/pkg10119023/latihan10/logika/IF110119023Latihan10Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119023.latihan10.logika;
+
+/**
+ *
+ * @author  
+ * NAMA     : Muhammad Farhan R
+ * KELAS    : IF1
+ * NIM      : 10119023
+ * Deskripsi Program : penggunaan logika pada java
+ */
+public class IF110119023Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : "+ dewasa);
+    }
+    
+}
